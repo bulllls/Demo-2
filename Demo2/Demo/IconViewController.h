bc535130aa09b213576e7d348a72312e404286eb
Ruslan <ruslanfilistovich@gmail.com>
2020-04-17 15:08:59 +0300

    start setting

---
+++ Demo2/Demo/IconViewController.h
@@ -0,0 +1,17 @@
+//
+//  IconViewController.h
+//  Demo2
+//
+//  Created by Ruslan on 4/17/20.
+//  Copyright © 2020 Ruslan Filistovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IconViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo2/Demo/IconViewController.m
@@ -0,0 +1,32 @@
+//
+//  IconViewController.m
+//  Demo2
+//
+//  Created by Ruslan on 4/17/20.
+//  Copyright © 2020 Ruslan Filistovich. All rights reserved.
+//
+
+#import "IconViewController.h"
+
+@interface IconViewController ()
+
+@end
+
+@implementation IconViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.whiteColor;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
